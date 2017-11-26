8b2f220693784d97581c6486db45e91f90ded3bc
Forcasius <forcasius_github@arcor.de>
2017-11-26 15:59:22 +0100

    we have dynamically drawn paths now
    * moved logic to a pathmanager that knows all paths relations
    * the paths are drawn in every frame now (could be optimized in the future)

--- Assets/Scripts/NodePathsManager.cs
+++ Assets/Scripts/NodePathsManager.cs
@@ -1,83 +1,116 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework;
+
+/// <summary>
+/// Draws the paths between the nodes.
+/// 
+/// The NodeManager must be attached to some GameObject that also contains a "path"-Object.
+/// The "path" object only contains a line renderer. The "path" object will be 
+/// duplicated dynamically for each drawn line.
+/// 
+/// Optimizations:
+/// * draw only when a node was moved.
+/// </summary>
 
 [ExecuteInEditMode]
 public class NodePathsManager : MonoBehaviour {
 
     public GameObject path;
-
-    private List<GameObject> lineRendererContainer = new List<GameObject>();
-    private List<Node> nodeList = new List<Node>();
+   
+    private Dictionary<Node, List<Node> > pathStorage = new Dictionary<Node, List<Node> >();
 
     void Start()
     {
-        
+        ClearDrawnPaths();
     }
 
     void Update()
     {
-        UpdateNodeList();
         ClearDrawnPaths();
-        foreach (Node node in nodeList)
-        {
-            DrawPath(node);
-        }
+        DrawPaths();
     }
 
-    private void UpdateNodeList()
+    private void ClearDrawnPaths()
+    {
+        // We need to clean up the root, since the running mode and editor mode seem to use different members...
+        GameObject[] rootGameObjects = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+        foreach (GameObject gameObject in rootGameObjects)
+        {
+            if (gameObject.name == "path(Clone)")
+            {
+                DestroyImmediate(gameObject);
+            }
+        }
+
+        foreach (var myList in pathStorage.Values)
+        {
+            myList.Clear();
+        }
+        pathStorage.Clear();
+    }
+
+    private void DrawPaths()
     {
         GameObject[] rootGameObjects = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
         foreach (GameObject gameObject in rootGameObjects)
         {
             Node foundNode = gameObject.GetComponent<Node>();
-            if (foundNode && !nodeList.Contains(foundNode))
+            if (foundNode)
             {
-                nodeList.Add(foundNode); 
+                DrawSinglePath(foundNode); 
             }
         }
     }
 
-    private void ClearDrawnPaths()
+    private void DrawSinglePath(Node currentNode)
     {
-        foreach (GameObject renderer in lineRendererContainer)
+        if (currentNode.possibleDirections != null) // Sometimes the node is not yet fully created
         {
-            DestroyImmediate(renderer);
-        }
-        lineRendererContainer.Clear();
-    }
-
-    private bool DrawPath(Node myNode)
-    {
-        bool result = true;
-        try
-        {
-            foreach (Node otherNode in myNode.possibleDirections.Values)
+            foreach (var other in currentNode.possibleDirections.Values)
             {
-                if (otherNode && (otherNode != this))
+                if (other && (other != currentNode) && !LineAlreadyDrawn(other, currentNode))
                 {
-                    Vector3 opponentPosition = otherNode.transform.position;
-                    Vector3 myPosition = myNode.transform.position;
-                    drawLine(opponentPosition, myPosition);
+                    Vector3 opponentPosition = other.transform.position;
+                    Vector3 myPosition = currentNode.transform.position;
+                    DrawLine(opponentPosition, myPosition);
+                    UpdatePathStorage(currentNode, other);
                 }
             }
         }
-        catch
-        {
-            result = false;
-        }
-        return result;
     }
 
-    private void drawLine(Vector3 startPosition, Vector3 endPosition)
+    private void DrawLine(Vector3 startPosition, Vector3 endPosition)
     {
         GameObject newPath = Instantiate(path);
         LineRenderer newLineRenderer = newPath.GetComponent<LineRenderer>();
-        this.lineRendererContainer.Add(newPath);
-        //        newPath.hideFlags = HideFlags.HideInHierarchy;
         newLineRenderer.enabled = true;
+        newPath.hideFlags = HideFlags.HideInHierarchy | HideFlags.DontSave;
         Vector3[] points = new Vector3[2]{ startPosition, endPosition };
         newLineRenderer.SetPositions(points); 
     }
 
+    private bool LineAlreadyDrawn(Node other, Node currentNode)
+    {
+        bool result = false;
+        if (pathStorage.ContainsKey(other))
+        {
+            if (pathStorage[other] != null && pathStorage[other].Contains(currentNode))
+            {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    private void UpdatePathStorage(Node currentNode, Node other)
+    {
+        if (!pathStorage.ContainsKey(currentNode))
+        {
+            pathStorage.Add(currentNode, new List<Node>());
+        }
+        pathStorage[currentNode].Add(other);
+    }
 }
