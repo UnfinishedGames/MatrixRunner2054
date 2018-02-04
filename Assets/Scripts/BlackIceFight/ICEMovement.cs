d39bae4363a0fd66c950a6fdf8f16486e4284a1f
Markus Hofmann <markus.w.hofmann@protonmail.com>
2018-02-04 21:04:35 +0100

    First draft for BlackICE encounter scene
    * we have a draft for alle the objects we need
    * we have a scene draft
    * we have a movig ICE

---
+++ Assets/Scripts/BlackIceFight/ICEMovement.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ICEMovement : MonoBehaviour
+{
+    public float RangeX = 3.0f;
+    public float Speed = 1.0f;
+    private bool _isMoving = false;
+
+    private Vector3 _destination;
+
+    private float _speedModifier;
+
+    // Use this for initialization
+    void Start()
+    {
+        Random.InitState(Time.frameCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (!_isMoving)
+        {
+            _speedModifier = Random.Range(0.8f, 1.2f);
+            var formerDirection = Math.Sign(_destination.x);
+            _destination = new Vector3(Random.Range(-1 * RangeX, RangeX), 0, 0);
+            if (Math.Sign(_destination.x) == formerDirection)
+            {
+                _destination *= -1;
+            }
+
+            _isMoving = true;
+        }
+        else
+        {
+            transform.Translate(new Vector3(Math.Sign(_destination.x), 0, 0) * Time.deltaTime * Speed *
+                                _speedModifier);
+            if (OnDestination(_destination))
+            {
+                _isMoving = false;
+            }
+        }
+    }
+
+    private bool OnDestination(Vector3 destination)
+    {
+        const float
+            deltaValue = 0.01f; // used to avoid deadlocks when the object cannot be transformed that small value
+        var onDestination = false;
+        if (Math.Sign(_destination.x) == -1)
+        {
+            if (destination.x > (transform.position.x + deltaValue))
+            {
+                onDestination = true;
+            }
+        }
+        else
+        {
+            if (destination.x < (transform.position.x - deltaValue))
+            {
+                onDestination = true;
+            }
+        }
+
+        return onDestination;
+    }
+}
