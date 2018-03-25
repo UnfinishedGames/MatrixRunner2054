6af89943ec9d78dd02d9db4eb99c26dd5c57f13d
Markus Hofmann <markus.w.hofmann@protonmail.com>
2018-03-25 22:09:21 +0200

    Introducing DeveloperOverride for BlackIceFight
    * RCtrl+W Wins the encounter
    * RCtlr+L Loses the encounter

---
+++ Assets/Scripts/BlackIceFight/DeveloperOverride.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeveloperOverride : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightControl))
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                Debug.Log("DeveloperOverride RCtrl+W");
+                PersistentEncounterStatus.Instance.status = EncounterStatus.PlayerWins;
+            }
+            else if (Input.GetKey(KeyCode.L))
+            {
+                Debug.Log("DeveloperOverride RCtrl+L");
+                PersistentEncounterStatus.Instance.status = EncounterStatus.PlayerLost;
+            }
+        }
+    }
+}
