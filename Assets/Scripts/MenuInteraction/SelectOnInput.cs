63129eaba35da4b9b38d64b2511d27e21d158d48
GitInMaRepo <oliver.s.roth@gmail.com>
2018-05-30 23:04:51 +0200

    Functional menu buttons

---
+++ Assets/Scripts/MenuInteraction/SelectOnInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour
+{
+    public EventSystem EventSystem;
+    public GameObject SelectedObject;
+
+    private bool buttonSelected;
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0 && !buttonSelected)
+        {
+            EventSystem.SetSelectedGameObject(SelectedObject);
+            buttonSelected = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
