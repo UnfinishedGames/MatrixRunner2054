941bb47fd6fba36e036e6716b514da6a3467195c
Markus Hofmann <markus.w.hofmann@protonmail.com>
2018-06-08 18:52:14 +0200

    adding logic for minigame "FindMatch"
    * the path for the resource file for the words is currently hard coded...
    * also adding tests

---
+++ Assets/Scripts/FindMatchHacking/FindMatchHackingLogic.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FindMatchHacking
+{
+    public class FindMatchHacking
+    {
+        /// <summary>
+        /// The length of the encrypted list
+        /// </summary>
+        public int _listLength = 1000;
+        /// <summary>
+        /// The searched word - that is the word the player needs to guess
+        /// </summary>
+        private string _searchedWord;
+        private Random _random;
+        /// <summary>
+        /// The final list to show the player
+        /// </summary>
+        private string _encryptedList = "";
+        /// <summary>
+        /// Characters used to obfuscate the words in the list
+        /// </summary>
+        private string _gibberish = "!§$%&/()=?*'#_-:.;,°^";
+
+        public string SearchedWord
+        {
+            get { return _searchedWord; }
+            set { _searchedWord = value; }
+        }
+
+        public FindMatchHacking(int seed)
+        {
+            _random = new Random(seed);
+        }
+
+        private string GetGibberishString(int length)
+        {
+            string result = "";
+            for (int index = 0; index < length; ++index)
+            {
+                result += _gibberish[_random.Next(_gibberish.Length)];
+            }
+
+            return result;
+        }
+        
+        private string GetNextWord(string[] wordList)
+        {
+            string nextWord = wordList[_random.Next(wordList.Length)];
+            if (nextWord == _searchedWord)
+            {
+                // Call again - its very unlikely that we get our searched word twice
+                nextWord = wordList[_random.Next(wordList.Length)];
+            }
+
+            return nextWord;
+        }
+        
+        public string GenerateList()
+        {
+            var wordList = ReadWordList();
+            _encryptedList = "";
+            _searchedWord = wordList[_random.Next(0, wordList.Length)];
+            while (_encryptedList.Length < _listLength)
+            {
+                _encryptedList += GetGibberishString(_random.Next(10, 50));
+                _encryptedList += GetNextWord(wordList);
+            }
+            
+            // Now we place our searched word in the list - we surround it with gibberish, just to be sure
+            int sizeOfSurrounding = 4;
+            var positionForSearchedWord = _random.Next(_listLength - _searchedWord.Length - (sizeOfSurrounding * 2));
+            string phraseToBePlaced = GetGibberishString(sizeOfSurrounding)
+                                      + _searchedWord
+                                      + GetGibberishString(sizeOfSurrounding);
+            _encryptedList = _encryptedList.Remove(positionForSearchedWord, phraseToBePlaced.Length);
+            return _encryptedList.Insert(positionForSearchedWord, phraseToBePlaced);
+        }
+
+        public string[] ReadWordList()
+        {
+            try
+            {
+                return File.ReadAllLines(@"/storage/projects/MatrixRunner2054/Resources/FindMatchHacking/top1000de_utf-8.txt");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public bool CheckPassword(string password)
+        {
+            return password == _searchedWord;
+        }
+
+        public string GetListOfMatchingChars(string password)
+        {
+            string result = "";
+            foreach (var character in password)
+            {
+                if (_searchedWord.Contains(character))
+                {
+                    result += character;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestProject/TestsFindMatchHacking.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Xml.Schema;
+using NUnit.Framework;
+using System.Linq;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestReadWordListNoThrow()
+        {
+            var testClass = new FindMatchHacking.FindMatchHacking(42);
+            Assert.DoesNotThrow(() => testClass.ReadWordList());
+        }
+
+        [Test]
+        public void TestReadWordList()
+        {
+            var testClass = new FindMatchHacking.FindMatchHacking(42);
+            var list = testClass.ReadWordList();
+            Assert.AreEqual(1000, list.Length);
+        }
+
+        [Test]
+        public void TestGenerateList()
+        {
+            var testClass = new FindMatchHacking.FindMatchHacking(42);
+            var list = testClass.GenerateList();
+            Assert.AreEqual("$*%/:*%:&/*(=/*um#=%§-;'!-%°-*%!(#;!'.:=%!§._$/.(_%!#*§=-_&ist/§:§?!.?**!;!!§_!°°?)#!)anderen.'=(°#:!,^(-%$?(.%$&#*^.-:(müßten_!)°!&/!&#=§^$$((:;!!;,&°;/'__&(%!&°^.$,%(''-$#=,Juni^?'°;$$!%°:=#=$°^,%)'#°Auto/)'./,_%/&;_§=.,.^°?,^%#&_*-$.)*:&'.#&°vor&(%(/,?)?._^Sonntag(%°##=^§?,(°,,!(#!,=-_^$'./jedem#§$__#(_$$;(stärker!#)%%_$°%_&__,)#==&.''-=Vater-&(';-(#:?^*,!==%'=!^?(*:=$%.?°°*&°,__;*(§$,_°'%ebenfalls)$!_!&°;;=#$.('oder_§'.':,-,)nehmen.;°;_§.°;%-^.;?°&/;)#°°§;);=$^$,*^!'_($&:$%?Ziel^-/^*#'!/,°,^(/=;,§;?;.,/,$_#?.?bereit)$$$**)?-%_.);^)//='$werde=:#*°?;$?&-°$(:)'*^%_/?'(_/%*!$%&&:),!?!,!%'-,,Das,,§$^($/§%/%__?§*:,')**!°(#$,;?.konnte#%!§&°#$;,&,&=:*%%^*&=)!%--!)&-?°!Sie!?*^^/-_;§§%==-(;:=%Beispiel,'&#:#&:^%Aus?-_!*°::&$#'_°')_%_,?!#°:)'§daran$,.°^^_;/?=§.Ob-^.§;$.'*!?$-/$=_($!:::^'-.°%?)&;!§=:!#^()(,',indem$!..-:&°&.*$(,)!-&/-%:_#-$-)=;?_Sprache;$-#*§;%%^'*-?^/&(#bleibt=)%§#!;*=(;)§!&)§-%;^'-§&?liegt%_%§?;?:''°!^_._-$#=.°$#Monaten=;-§,%.*):%.,/#?;'.%,):!=§;;Seiten''_!=#§?-./*(§*,&)Diskussion"
+            , list);
+        }
+        
+        [Test]
+        public void TestIsSearchedWordPartOfGenerateList()
+        {
+            var testClass = new FindMatchHacking.FindMatchHacking(42);
+            var list = testClass.GenerateList();
+            var searchedWord = testClass.SearchedWord;
+            Assert.IsTrue(list.Contains(searchedWord));
+        } 
+        
+        [Test]
+        public void TestCheckPassword()
+        {
+            var testClass = new FindMatchHacking.FindMatchHacking(42);
+            testClass.SearchedWord = "FooBarBaz";
+            Assert.IsTrue(testClass.CheckPassword("FooBarBaz"));
+            Assert.IsFalse(testClass.CheckPassword("FooBarbaz"));
+            Assert.IsFalse(testClass.CheckPassword("FooBarBazboo"));
+            Assert.IsFalse(testClass.CheckPassword("Foo"));
+            Assert.IsFalse(testClass.CheckPassword("feFooBarBaz"));
+        }        
+        
+        [Test]
+        public void TestGetListOfMatchingChars()
+        {
+            var testClass = new FindMatchHacking.FindMatchHacking(42);
+            testClass.SearchedWord = "FooBarBaz";
+            Assert.AreEqual("ao", testClass.GetListOfMatchingChars("Hallo"));
+            Assert.AreEqual("", testClass.GetListOfMatchingChars("Nichts"));
+            Assert.AreEqual("Foo", testClass.GetListOfMatchingChars("Foo"));
+            Assert.AreEqual("Bao", testClass.GetListOfMatchingChars("Bahnhof"));
+            Assert.AreEqual("FooBarBaz", testClass.GetListOfMatchingChars("FooBarBaz"));
+        }
+    }
+}
