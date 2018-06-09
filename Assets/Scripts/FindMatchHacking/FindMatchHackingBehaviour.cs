23ad360f913f4e7756a5884b18e4e597e064423d
Markus Hofmann <markus.w.hofmann@protonmail.com>
2018-06-09 12:14:47 +0200

    finalizing game logic for FindMatch
    * more intense GUI (with hex field and line numbers)
    * changing font for readability
    * using resource system of unity for the loading of the text file
    * adding time pressure

--- Assets/Scripts/FindMatchHacking/FindMatchHackingBehaviour.cs
+++ Assets/Scripts/FindMatchHacking/FindMatchHackingBehaviour.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 using FindMatchHacking;
@@ -8,17 +10,51 @@ using FindMatchHacking;
 public class FindMatchHackingBehaviour : MonoBehaviour
 {
 	public Text TextWindow;
+	public Text HexWindow;
+	public Text SideWindow;
 	public InputField InputBox;
-
+	public TextAsset WordList;
+	public Slider TimerSlider;
+	public float TimeInSeconds = 30;
+	
 	private FindMatchHacking.FindMatchHacking _hackingLogic;
+	private float StartTime = 0;
+	
 	// Use this for initialization
-	void Start () 
+	void Start ()
 	{
-		_hackingLogic = new FindMatchHacking.FindMatchHacking((int)(Time.time*100.0f));
+		StartTime = Time.time;
+		var seed = (int) (Time.time * 100.0f);
+		_hackingLogic = new FindMatchHacking.FindMatchHacking(seed, WordList.text.Split('\n'));
 		
 		if (TextWindow)
 		{
 			TextWindow.text = _hackingLogic.GenerateList();
+		}
+
+		if (HexWindow)
+		{
+			HexWindow.text = "";
+			foreach (var character in _hackingLogic.EncryptedList)
+			{
+				char[] characterArray = {character};
+				byte[] ba = Encoding.UTF8.GetBytes(characterArray);
+				var hexString = BitConverter.ToString(ba).Substring(0,2);
+				HexWindow.text += hexString + " ";
+			}
+
+//			var hexString = BitConverter.ToString(ba);
+//			hexString = hexString.Replace("-", " ");
+//			HexWindow.text = hexString;
+		}
+
+		if (SideWindow)
+		{
+			Canvas.ForceUpdateCanvases();
+			for (int line = 0; line < TextWindow.cachedTextGenerator.lines.Count; ++line)
+			{
+				SideWindow.text += "0x" + line.ToString("X4") + "\n";
+			}
 		}
 
 		if (InputBox)
@@ -28,27 +64,44 @@ public class FindMatchHackingBehaviour : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () 
+	{
+		if (TimerSlider)
+		{
+			TimerSlider.value = 1.0f - (Time.time - StartTime) / TimeInSeconds;
+		}
+
+		if (Time.time - StartTime > TimeInSeconds)
+		{
+			Debug.Log("You Loose");
+		}
 	}
 
 	public void CheckInput(string input)
 	{
 		TextWindow.text = "";
+		HexWindow.text = "";
 		var charList = _hackingLogic.GetListOfMatchingChars(input);
 
 		foreach (var character in _hackingLogic.EncryptedList)
 		{
+			char[] characterArray = {character};
+			byte[] ba = Encoding.UTF8.GetBytes(characterArray);
+			var hexString = BitConverter.ToString(ba).Substring(0,2);
+			
 			if (charList.Contains(character))
 			{
 				TextWindow.text += "<color=red>" + character + "</color>";
+				HexWindow.text += "<color=red>" + hexString + "</color>" + " ";
 			}
 			else
 			{
 				TextWindow.text += character;
+				HexWindow.text += hexString + " ";
 			}
 		}
 
+		HexWindow.text = HexWindow.text.TrimEnd();
 
 		if (_hackingLogic.CheckPassword(input))
 		{
--- Assets/Scripts/FindMatchHacking/FindMatchHackingLogic.cs
+++ Assets/Scripts/FindMatchHacking/FindMatchHackingLogic.cs
@@ -11,14 +11,27 @@ namespace FindMatchHacking
         /// The length of the encrypted list
         /// </summary>
         public int _listLength = 1000;
-        
+
+        /// <summary>
+        /// The List of words to choose from
+        /// </summary>
+        private string[] _words;
+
+        /// <summary>
+        /// The list of words that are present in the current game
+        /// </summary>
         private List<string> currentWords = new List<string>();
+
+        /// <summary>
+        /// Used to generate random values, for example to access the word list
+        /// </summary>
+        private Random _random;
 
         /// <summary>
         /// The searched word - that is the word the player needs to guess
         /// </summary>
         private string _searchedWord;
-        private Random _random;
+
         /// <summary>
         /// The final list to show the player
         /// </summary>
@@ -28,28 +41,37 @@ namespace FindMatchHacking
         /// Characters used to obfuscate the words in the list
         /// </summary>
         private string _gibberish = "!§$%&/()=?*'#_-:.;,°^0123456789";
-        
+
+        #region Properties Region
+
         public List<string> CurrentWords
         {
             get { return currentWords; }
         }
-        
+
         public string SearchedWord
         {
             get { return _searchedWord; }
             set { _searchedWord = value; }
         }
 
-        public FindMatchHacking(int seed)
-        {
-            _random = new Random(seed);
-        }
-
         public string EncryptedList
         {
             get { return _encryptedList; }
         }
-        
+
+        #endregion
+
+        public FindMatchHacking(int seed, string[] words)
+        {
+            _random = new Random(seed);
+            _words = words;
+            for (int index = 0; index < _words.Length; ++index)
+            {
+                _words[index] = _words[index].ToLower().Trim();
+            }
+        }
+
         private string GetGibberishString(int length)
         {
             string result = "";
@@ -60,7 +82,7 @@ namespace FindMatchHacking
 
             return result;
         }
-        
+
         private string GetNextWord(string[] wordList)
         {
             string nextWord = wordList[_random.Next(wordList.Length)];
@@ -69,54 +91,60 @@ namespace FindMatchHacking
                 // Call again - its very unlikely that we get our searched word twice
                 nextWord = wordList[_random.Next(wordList.Length)];
             }
+
             currentWords.Add(nextWord);
             return nextWord;
         }
-        
+
         public string GenerateList()
         {
-            var wordList = ReadWordList();
             _encryptedList = "";
-            _searchedWord = wordList[_random.Next(0, wordList.Length)];
-            while (_encryptedList.Length < _listLength)
+            if (_words != null)
             {
-                _encryptedList += GetGibberishString(_random.Next(10, 50));
-                _encryptedList += GetNextWord(wordList);
+                _searchedWord = _words[_random.Next(0, _words.Length)];
+                while (_encryptedList.Length < _listLength)
+                {
+                    _encryptedList += GetGibberishString(_random.Next(10, 50));
+                    _encryptedList += GetNextWord(_words);
+                }
+
+                // Now we place our searched word in the list - we surround it with gibberish, just to be sure
+                int sizeOfSurrounding = 4;
+                var positionForSearchedWord =
+                    _random.Next(_listLength - _searchedWord.Length - (sizeOfSurrounding * 2));
+                string phraseToBePlaced = GetGibberishString(sizeOfSurrounding)
+                                          + _searchedWord
+                                          + GetGibberishString(sizeOfSurrounding);
+                _encryptedList = _encryptedList.Remove(positionForSearchedWord,
+                    phraseToBePlaced.Length + sizeOfSurrounding * 2);
+                _encryptedList = _encryptedList.Insert(positionForSearchedWord, phraseToBePlaced);
             }
-            
-            // Now we place our searched word in the list - we surround it with gibberish, just to be sure
-            int sizeOfSurrounding = 4;
-            var positionForSearchedWord = _random.Next(_listLength - _searchedWord.Length - (sizeOfSurrounding * 2));
-            string phraseToBePlaced = GetGibberishString(sizeOfSurrounding)
-                                      + _searchedWord
-                                      + GetGibberishString(sizeOfSurrounding);
-            _encryptedList = _encryptedList.Remove(positionForSearchedWord, phraseToBePlaced.Length + sizeOfSurrounding * 2);
-            _encryptedList = _encryptedList.Insert(positionForSearchedWord, phraseToBePlaced);
+
             return _encryptedList;
         }
 
-        public string[] ReadWordList()
-        {
-            try
-            {
-                return File.ReadAllLines(@"/storage/projects/MatrixRunner2054/Resources/FindMatchHacking/top1000de_utf-8.txt");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-        }
+//        public string[] ReadWordList()
+//        {
+//            try
+//            {
+//                return File.ReadAllLines(@"/storage/projects/MatrixRunner2054/Resources/FindMatchHacking/top1000de_utf-8.txt");
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine(e);
+//                throw;
+//            }
+//        }
 
         public bool CheckPassword(string password)
         {
-            return password == _searchedWord;
+            return password.ToLower() == _searchedWord;
         }
 
         public string GetListOfMatchingChars(string password)
         {
             string result = "";
-            foreach (var character in password)
+            foreach (var character in password.ToLower())
             {
                 if (_searchedWord.Contains(character))
                 {
