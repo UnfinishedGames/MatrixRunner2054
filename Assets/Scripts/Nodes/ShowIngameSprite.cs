0b38f29d910fd6495de2647d787c9c6fc485e7ab
GitInMaRepo <oliver.s.roth@gmail.com>
2018-07-09 20:52:23 +0200

    Added a placeholder for a possible 3-d version of the game board

--- Assets/Scripts/Nodes/ShowIngameSprite.cs
+++ Assets/Scripts/Nodes/ShowIngameSprite.cs
@@ -1,22 +1,33 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ShowIngameSprite : MonoBehaviour
 {
     public Sprite SpecificImage;
-
     private SpriteRenderer mySprite;
+    private ShowMesh myMeshScript;
 
     // Use this for initialization
     void Start()
     {
         mySprite = GetComponentInChildren<SpriteRenderer>();
         mySprite.enabled = true;
+        
+        var parentMesh = transform.Find("Tower");
+        if (parentMesh == null) return;
+        var mesh = parentMesh.Find("Tower.Shape");
+        if (mesh == null) return;
+        var script = mesh.GetComponentInChildren<ShowMesh>();
+        if (script == null) return;
+        myMeshScript = script;
     }
 
     public void SwitchSpecificSpriteOn()
     {
         mySprite.sprite = SpecificImage;
+        if(myMeshScript != null)
+        {
+            myMeshScript.SetVisible();
+        }
     }
 
     public void SetSpriteColor(Color color)
---
+++ Assets/StepsTowards3d/ShowMesh.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ShowMesh : MonoBehaviour
+{
+    MeshRenderer myRenderer;
+    float rotationSpeed = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRenderer = GetComponentInChildren<MeshRenderer>();
+        myRenderer.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetVisible()
+    {
+        myRenderer.gameObject.SetActive(true);
+    }
+}
